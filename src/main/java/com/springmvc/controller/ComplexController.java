183a1bbbff013cb1b627430f85bcc7a0ca60e6d8
krtuhin <tuhin.km2774@gmail.com>
2023-07-26 17:13:20 +0530

    Use of PathVariable Annotation

--- src/main/java/com/springmvc/controller/ComplexController.java
+++ src/main/java/com/springmvc/controller/ComplexController.java
@@ -4,6 +4,7 @@ import com.springmvc.models.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,12 +22,22 @@ public class ComplexController {
     public String submit(@ModelAttribute Student student, BindingResult result) {
 
         //checking form errors
-        if(result.hasErrors()){
+        if (result.hasErrors()) {
             return "complex_form";
         }
-        
+
         System.out.println(student);
 
         return "show";
+    }
+
+    //use of @PathVariable annotation
+    @RequestMapping("/user/{userId}/{name}")
+    public String path(@PathVariable("userId") int userId, @PathVariable("name") String name) {
+
+        System.out.println("User id : " + userId);
+        System.out.println("Name : " + name);
+
+        return "complex_form";
     }
 }
